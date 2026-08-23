f7c52cb46242387cb3f14e00c9c4f9ae571403b0
AldairAlburqueque <aldairalburqueque8@gmail.com>
2026-08-23 16:47:54 -0500

    mejora del reproductor de video

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
-import './style/view.css'
+import React from "react";
+import "./style/view.css";
 
 const View = ({ video, setWatch }) => {
-
   const handleView = () => {
-    setWatch(false)
-  }
+    setWatch(false);
+  };
+
+  const handleBackgroundClick = (e) => {
+    if (e.target === e.currentTarget) {
+      setWatch(false);
+    }
+  };
 
   return (
-    <div className='view_info'>
-                {
-              video?.results.slice(0,1).map(v => (
-                <div className="view_card" key={v.id}>
-                  <iframe
-                    src={`https://www.youtube.com/embed/${v.key}`} 
-                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    title="YouTube video player">
-                  </iframe>
-                  
-                </div>
-              ))
-            }
-            <i class='bx bx-x-circle' onClick={handleView}></i>
-    </div>
-  )
-}
+    <div className="view_info" onClick={handleBackgroundClick}>
+      <div className="view_card">
+        {video?.results?.slice(0, 1).map((v) => (
+          <iframe
+            key={v.id}
+            src={`https://www.youtube.com/embed/${v.key}?autoplay=1`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        ))}
 
-export default View
+        <button
+          className="view_close"
+          onClick={handleView}
+          aria-label="Cerrar reproductor"
+        >
+          <i className="bx bx-x"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default View;
