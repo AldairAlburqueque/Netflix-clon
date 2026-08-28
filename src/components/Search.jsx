cd331c44eb31e1fe5c4454b119618c8cf9f18972
AldairAlburqueque <aldairalburqueque8@gmail.com>
2026-08-27 22:18:02 -0500

    limpiando codigo comentado

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,3 @@
-// import React, { useState } from 'react'
-// import './style/search.css'
-
-// const Search = ({ onSearch }) => {
-
-//   const [query, setQuery] = useState('');
-
-//   const handleSubmit = e => {
-//     e.preventDefault();
-//     setQuery(e.target.query.value.trim().toLowerCase());
-//     //
-//     onSearch(query)
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit} className='search'>
-//       <input className='input_search' type="text"  id='query'/>
-//       <button className='buttom_search'><i className='bx bx-search-alt-2'></i></button>
-//     </form>
-//   )
-// }
-
-// export default Search
-
 import React, { useState } from "react";
 import "./style/search.css";
 
